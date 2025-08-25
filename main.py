34c93b881f6df516d4450f8b17df22dec626eb5d
Zakaria742 <zaakizakaria12@gmail.com>
2025-08-25 19:31:27 +0100

    .

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='static',
+            static_url_path='/static')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():  
         return render_template("index.html")
 
-app.run(debug=True)
+@app.route('/static/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory('static/assets', filename)
+
+def handler(event=None, context=None):
+        return app
+
+if __name__ == "__main__":
+        app.run(debug=True)
